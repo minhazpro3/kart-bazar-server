84ac4ced8d1eb8f077d5f152c3a543680dc9fc27
minhazpro3 <minhazpro1@gmail.com>
2023-04-03 18:49:22 +0600

    signup user done

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,72 +2,81 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 var jwt = require("jsonwebtoken");
 
-const signupSchema = require("../schemas/userSchema");
-const User = new mongoose.model("users", signupSchema);
-
+const SignupModel = require("../schemas/userSchema");
 // user signup
 exports.signUp = async (req, res, next) => {
   try {
     // password hashed
     const hashPassword = await bcrypt.hash(req.body.password, 10);
     // users objects data
-    const newUser = new User({
-      name: req.body.name,
+
+    const signupModel = new SignupModel({
       email: req.body.email,
       password: hashPassword,
     });
 
-    const result = await newUser.save();
-    //response from
-    res.status(200).send({
-      status: "success!",
-      message: result,
-    });
+    const existMail = await SignupModel.find({ email: req.body.email });
+    console.log(existMail[0]);
+
+    if (existMail[0]?.email) {
+      return res.status(401).json({
+        status: false,
+        message: "User already exist",
+      });
+    }
+
+    if (hashPassword) {
+      const result = await signupModel.save();
+
+      res.send(result);
+    }
+
     // any error
   } catch (error) {
-    res.status(400).send({
+    res.status(401).send({
       status: "failed!",
-      message: error.message,
+      message: "Authentication failed",
     });
+    console.log(error.message);
   }
 };
 
 // signIn user
-exports.signIn = async (req, res, next) => {
-  try {
-    const isUser = await User.find({ email: req.body.email });
-    if (isUser.length > 0) {
-      const isValidation = await bcrypt.compare(
-        req.body.password,
-        isUser[0].password
-      );
-      if (isValidation) {
-        // generate token
+// exports.signIn = async (req, res, next) => {
+//   try {
+//     const isUser = await User.find({ email: req.body.email });
+//     if (isUser.length > 0) {
+//       const isValidation = await bcrypt.compare(
+//         req.body.password,
+//         isUser[0].password
+//       );
+//       if (isValidation) {
+//         // generate token
 
-        const token = jwt.sign(
-          {
-            name: isUser[0].name,
-            userId: isUser[0]._id,
-          },
-          process.env.JWT_Sk
-        );
+//         const token = jwt.sign(
+//           {
+//             name: isUser[0].name,
+//             userId: isUser[0]._id,
+//           },
+//           process.env.JWT_Sk
+//         );
 
-        res.status(200).send({
-          status: true,
-          access_token: token,
-          message: "Login success",
-        });
-      }
-    } else {
-      res.status(401).json({
-        status: false,
-        message: "Authenticate fail!",
-      });
-    }
-  } catch (error) {
-    res.status(401).json({
-      status: false,
-      message: "Authenticate fail!",
-    });
-  }
-};
+//         res.status(200).send({
+//           status: true,
+//           access_token: token,
+//           message: "Login success",
+//         });
+//       }
+//     } else {
+//       res.status(401).json({
+//         status: false,
+//         message: "Authenticate fail!",
+//       });
+//     }
+//   } catch (error) {
+//     res.status(401).json({
+//       status: false,
+//       message: "Authenticate fail!",
+//     });
+//   }
+// };
--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -3,21 +3,12 @@ const validator = require("validator");
 
 // signup schema
 const signupSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"],
-    trim: true,
-    unique: [true, "Name must be unique"],
-    lowercase: true,
-    maxLength: 30,
-    minLength: 3,
-  },
   email: {
     type: String,
     required: [true, "Email address is required"],
     validate: [validator.isEmail, "Please provide a valid E-mail"],
     trim: true,
-    unique: [true, "Email must be unique"],
+    unique: [true, "Please provide another email address"],
     lowercase: true,
     maxLength: 30,
     minLength: [8, "please provide min 15 length"],
@@ -26,30 +17,30 @@ const signupSchema = mongoose.Schema({
     type: String,
     required: [true, "Password is required"],
     trim: true,
-    minLength: 8,
+    minLength: [8, "Please provide min 8 length password"],
   },
 });
 
-module.exports = signupSchema;
+const signupModel = mongoose.model("users", signupSchema);
+module.exports = signupModel;
 
 // signIn schema
-const signInSchema = mongoose.Schema({
-  email: {
-    type: String,
-    required: [true, "Email address is required"],
-    validate: [validator.isEmail, "Please provide a valid E-mail"],
-    trim: true,
+// const signInSchema = mongoose.Schema({
+//   email: {
+//     type: String,
+//     required: [true, "Email address is required"],
+//     validate: [validator.isEmail, "Please provide a valid E-mail"],
+//     trim: true,
+//     lowercase: true,
+//     maxLength: 30,
+//     minLength: [8, "please provide min 8 length"],
+//   },
+//   password: {
+//     type: String,
+//     required: [true, "Password is required"],
+//     trim: true,
+//     minLength: 8,
+//   },
+// });
 
-    lowercase: true,
-    maxLength: 30,
-    minLength: [8, "please provide min 8 length"],
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-    trim: true,
-    minLength: 8,
-  },
-});
-
-module.exports = signInSchema;
+// module.exports = signInSchema;
